7f634c0d73f3225ded841252eef6a4cb5ef78369
jingzhao <jingzhao@thoughtworks.com>
2016-07-20 17:34:16 +0800

    add UserFooterInfoCell

---
+++ weibo_jingzhao/Home/UserFooterInfoCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserFooterInfoCell.swift
+//  weibo_jingzhao
+//
+//  Created by JingZhao on 7/20/16.
+//  Copyright © 2016 JingZhao. All rights reserved.
+//
+
+import Foundation
+
+class UserFooterInfoCell: UIView {
+    
+    
+}
