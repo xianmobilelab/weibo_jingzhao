f248860d5a4d1eb089acd3e1a1222b179a8b9233
jingzhao <jingzhao@thoughtworks.com>
2016-07-20 17:52:02 +0800

    have picture type cell

--- weibo_jingzhao/Home/StatusTextPicCell.swift
+++ weibo_jingzhao/Home/StatusTextPicCell.swift
@@ -8,14 +8,51 @@
 
 import Foundation
 
-class StatusTextPicCell: UITableViewCell {
+class StatusTextPicCell: StatusCell {
     static let identifierCell = "identifierStatusTextPicCell"
+    var headerView:UserHeaderInfoCell?
+    var footerView:UserFooterInfoCell?
+    var picView = PicViewCell()
     
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?){
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    override func configureData(statusModel: StatusModel) {
+        guard let headerView = headerView else {
+            return
+        }
+        headerView.configureData(statusModel.userModel)
+        contentLabel.text = statusModel.text
+        picView.configurePic(statusModel.picUrls)
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        addSubviewNeed()
+        setUpLayout()
     }
+    
+    func addSubviewNeed() {
+        headerView =  addHeaderView()
+        footerView = addFooterView()
+        textContentCell()
+        addPicView()
+    }
+    
+    func addPicView() {
+        picView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(picView)
+    }
+    
+    func setUpLayout() {
+        guard let headerView = headerView, let footerView = footerView else {
+            return
+        }
+        addConstraits("H:|[header]|", views:["header":headerView])
+        addConstraits("V:|[header(==60)]", views: ["header":headerView])
+        addConstraits("H:|[text]|", views: ["text":contentLabel])
+        addConstraits("V:[header][text]", views: ["header":headerView,"text":contentLabel])
+        addConstraits("H:|[picView]", views: ["picView":picView])
+        addConstraits("V:[text][picView]", views: ["text":contentLabel, "picView":picView])
+        addConstraits("H:|[footer]|", views: ["footer":footerView])
+        addConstraits("V:[picView][footer]|", views: ["picView":picView, "footer":footerView])
+    }
+    
 }
